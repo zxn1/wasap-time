6134ec552a688c29b75bf104df6cfe518e66008c
Muhd Jamil Husni <maintanance1991@gmail.com>
2023-04-21 10:52:32 +0800

    fix button call and chat

--- resources/views/livewire/friendlist.blade.php
+++ resources/views/livewire/friendlist.blade.php
@@ -25,7 +25,7 @@
                     <span class="name">{{ $list->name }}</span><br>
                     <span style="font-size : 7px;">{{ substr($list->session_id, 0, 15) . '...' }}</span>
                 </div>
-                <div style="margin-left: auto;">
+                <div style="margin-left: auto; width : 165px; min-width : 165px;">
                     <button class="call-btn">Call&nbsp;
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-camera-video-fill" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M0 5a2 2 0 0 1 2-2h7.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 4.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 13H2a2 2 0 0 1-2-2V5z"/>
